8df909973f78beecfcafd48a8d81f39dada8f552
Sylvain Klam <sylvain.klam@gmail.com>
2020-03-20 22:56:18 +0100

    pascal triangle

---
+++ ExercisesInJava/src/sdk/exercises/fr/paris/E04PascalTriangle.java
@@ -0,0 +1,31 @@
+package sdk.exercises.fr.paris;
+
+import java.util.Scanner;
+
+/**
+ * Écrire un programme permettant d’afficher un triangle pour une hauteur
+ * donnée.
+ * 
+ * @author sdklm
+ *
+ */
+public class E04PascalTriangle {
+	@SuppressWarnings("resource")
+	public static void main(String args[]) {
+		int r, i, k, number = 1, j;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter Number of Rows : ");
+		r = scan.nextInt();
+		for (i = 0; i < r; i++) {
+			for (k = r; k > i; k--) {
+				System.out.print(" ");
+			}
+			number = 1;
+			for (j = 0; j <= i; j++) {
+				System.out.print("* ");
+				number = number * (i - j) / (j + 1);
+			}
+			System.out.println();
+		}
+	}
+}
